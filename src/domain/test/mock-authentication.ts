7057d5c60203b58562be9c90c0b4caf591ef7b74
Carolina Gonzalez <carolina.gonzalez@mercadolivre.com>
2021-10-05 23:29:50 -0300

    test: ensure RemoteAuthentication calls HttpPOstClinent with the correct body

---
+++ src/domain/test/mock-authentication.ts
@@ -0,0 +1,10 @@
+import {
+    Authentication,
+    AuthenticationParams,
+} from '../usecases/authentication'
+import faker from 'faker'
+
+export const mockAuthentication = (): AuthenticationParams => ({
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+})
